67cd6a5a4c81baec39de769f22668d9296178d37
John E Vecchio <johnvecchio24@gmail.com>
2017-08-07 19:00:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function main() {
-  $('.jumbotron').hide();
-  $('.jumbotron').fadeIn(2500);
-}
+// var dvs = document.querySelectorAll(".hideme");
+// var cts = document.querySelectorAll(".lbl");
 
-$(document).ready(main);
+// for (var i = 0; i < dvs.length; i++) {
+// 	// console.log(dvs[i]);
+// 	dvs[i].classList.toggle("hideSection");
+// }
+
+// for (var i = 0; i < cts.length; i++) {
+// 	cts[i].addEventListener("click", function(){
+// 		console.log(i);
+// 		// dvs[i].classList.toggle("hideSection");
+// 	});
+// }
+
+  $(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('fast');
+    });
+  });
