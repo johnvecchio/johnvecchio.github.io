ab6b67fb6c6de28b465e7aa363a025e2a686a9f3
John E Vecchio <johnvecchio24@gmail.com>
2017-08-08 21:43:06 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function($) {
 	$('#accordion').find('.accordion-toggle').click(function(){
 	  //Expand or collapse this panel
-	  $(this).next().slideToggle('fast');
+	  $(this).next().slideToggle();
 	  //Hide the other panels
 	  $(".accordion-content").not($(this).next()).slideUp('fast');
 	});
